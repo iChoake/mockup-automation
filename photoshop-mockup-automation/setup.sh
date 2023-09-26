51f58b74d84d4a00174f08120bd017e9254f5f14
Steven <45870644+iChoake@users.noreply.github.com>
2023-09-26 09:06:57 -0400

    Create setup.sh

---
+++ photoshop-mockup-automation/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Setting up Photoshop Mockup Automation..."
+
+# Clone the GitHub repo
+git clone https://github.com/iChoake/photoshop-mockup-automation
+cd photoshop-mockup-automation
+
+# Create required directories if they don't exist
+mkdir -p output
+
+echo "Setup complete. Ready to run Photoshop scripts
